250017fb5994de508b62ea5071d44f45acc2bcd9
sumit jangir <sumitjangir179@gmail.com>
2023-11-13 12:54:41 +0530

    refact: code refactor of  Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
         <Container>
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold hover:text-gray-500"> Login to read posts </h1>
+              <h1 className="text-2xl font-bold hover:text-gray-500"> No Blog Post </h1>
             </div>
           </div>
         </Container>
